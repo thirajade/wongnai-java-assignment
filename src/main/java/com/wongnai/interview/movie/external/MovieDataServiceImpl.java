6bf8b4d21f18c77305d596fbd2f4becf35715d7d
Thirajade Lekkunlawat <thirajade.lkw@gmail.com>
2019-08-27 00:06:45 +0700

    Add: fetchAll method

--- src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
+++ src/main/java/com/wongnai/interview/movie/external/MovieDataServiceImpl.java
@@ -1,10 +1,13 @@
 package com.wongnai.interview.movie.external;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
 
 @Component
 public class MovieDataServiceImpl implements MovieDataService {
@@ -23,7 +26,14 @@ public class MovieDataServiceImpl implements MovieDataService {
 		// Step 1 => Implement this method to download data from MOVIE_DATA_URL and fix any error you may found.
 		// Please noted that you must only read data remotely and only from given source,
 		// do not download and use local file or put the file anywhere else.
+        String jsonResponse = restTemplate.getForEntity(this.MOVIE_DATA_URL, String.class).getBody();
+        MoviesResponse moviesResponse = new MoviesResponse();
+        try {
+            moviesResponse = objectMapper.readValue(jsonResponse, MoviesResponse.class);
+        } catch (IOException ioException) {
+            //will implement exponential backoff later.
+        }
 
-		return null;
+		return moviesResponse;
 	}
 }
