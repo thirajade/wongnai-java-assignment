7ad3888c082a1b80867b1903e2b7d98eb57ab252
Thirajade Lekkunlawat <thirajade.lkw@gmail.com>
2019-08-27 11:23:03 +0700

    Add: forceSync() method for syncing the movie data when start server

--- src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
+++ src/main/java/com/wongnai/interview/movie/sync/MovieDataSynchronizer.java
@@ -2,11 +2,18 @@ package com.wongnai.interview.movie.sync;
 
 import javax.transaction.Transactional;
 
+import com.wongnai.interview.movie.Movie;
+import com.wongnai.interview.movie.external.MovieData;
+import com.wongnai.interview.movie.external.MoviesResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.wongnai.interview.movie.MovieRepository;
 import com.wongnai.interview.movie.external.MovieDataService;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Component
 public class MovieDataSynchronizer {
@@ -19,5 +26,11 @@ public class MovieDataSynchronizer {
 	@Transactional
 	public void forceSync() {
 		//TODO: implement this to sync movie into repository
+		MoviesResponse moviesResponse = movieDataService.fetchAll();
+		for (MovieData movieData : moviesResponse) {
+			Movie movie = new Movie(movieData.getTitle());
+			movie.getActors().addAll(movieData.getCast());
+			movieRepository.save(movie);
+		}
 	}
 }
