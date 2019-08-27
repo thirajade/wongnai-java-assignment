c24d37cd70cc9a9c5f96da66226bfd5e9cc8d708
Thirajade Lekkunlawat <thirajade.lkw@gmail.com>
2019-08-27 16:36:43 +0700

    Add: RequestParam "q" validation and MoviesControllerAdvice for handling validation error

---
+++ src/main/java/com/wongnai/interview/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.wongnai.interview.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
+}
--- src/main/java/com/wongnai/interview/movie/MoviesController.java
+++ src/main/java/com/wongnai/interview/movie/MoviesController.java
@@ -1,39 +1,42 @@
 package com.wongnai.interview.movie;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
 @RestController
 @RequestMapping("/movies")
+@Validated
 public class MoviesController {
-	/**
-	 * Inject movie search service and use to handle search request.
-	 * <p>
-	 * You can specify an implementation using @Qualifier("beanName"), for example:
-	 *
-	 * <pre>
-	 * {@literal @}Qualifier("databaseMovieSearchService")
-	 * {@literal @}Qualifier("simpleMovieSearchService")
-	 * {@literal @}Qualifier("invertedIndexMovieSearchService")
-	 * </pre>
-	 */
-	@Autowired
-	@Qualifier("invertedIndexMovieSearchService")
-	private MovieSearchService movieSearchService;
+    /**
+     * Inject movie search service and use to handle search request.
+     * <p>
+     * You can specify an implementation using @Qualifier("beanName"), for example:
+     *
+     * <pre>
+     * {@literal @}Qualifier("databaseMovieSearchService")
+     * {@literal @}Qualifier("simpleMovieSearchService")
+     * {@literal @}Qualifier("invertedIndexMovieSearchService")
+     * </pre>
+     */
+    @Autowired
+    @Qualifier("invertedIndexMovieSearchService")
+    private MovieSearchService movieSearchService;
 
-	@RequestMapping(method = RequestMethod.GET)
-	public String helloWorld() {
-		return "Hello World!";
-	}
+    @RequestMapping(method = RequestMethod.GET)
+    public String helloWorld() {
+        return "Hello World!";
+    }
 
-	@RequestMapping(value = "/search", method = RequestMethod.GET)
-	public List<Movie> searchTitleWithKeyword(@RequestParam("q") String keyword) {
-		return movieSearchService.search(keyword);
-	}
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public List<Movie> searchTitleWithKeyword(@RequestParam(name = "q") @NotBlank(message = "q cannot be blank") String keyword) {
+        return movieSearchService.search(keyword);
+    }
 }
