6423ef9944a3f55e1763d8d8344cd4e0828b2211
Thirajade Lekkunlawat <thirajade.lkw@gmail.com>
2019-08-29 22:07:09 +0700

    Add: @AutoConfigureMockMvc for other test classes

--- src/test/java/com/wongnai/interview/movie/external/MovieDataServiceImplIntegrationTest.java
+++ src/test/java/com/wongnai/interview/movie/external/MovieDataServiceImplIntegrationTest.java
@@ -1,17 +1,19 @@
 package com.wongnai.interview.movie.external;
 
-import java.util.Arrays;
-import java.util.Optional;
-
 import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 @SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringJUnit4ClassRunner.class)
 public class MovieDataServiceImplIntegrationTest {
 	@Autowired
--- src/test/java/com/wongnai/interview/movie/search/SimpleMovieSearchServiceIntegrationTest.java
+++ src/test/java/com/wongnai/interview/movie/search/SimpleMovieSearchServiceIntegrationTest.java
@@ -7,12 +7,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.List;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SimpleMovieSearchServiceIntegrationTest {
 	@Autowired
