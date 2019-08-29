a80ae9c95a5bc0f35c60f67d3aeafed719caf700
Thirajade Lekkunlawat <thirajade.lkw@gmail.com>
2019-08-29 22:05:44 +0700

    Add: testSearchWithEmptyString() for test when request parameter q is an empty string

--- src/test/java/com/wongnai/interview/movie/MoviesControllerIntegrationTest.java
+++ src/test/java/com/wongnai/interview/movie/MoviesControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.wongnai.interview.movie;
 
-import java.util.Arrays;
-
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +8,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,4 +31,11 @@ public class MoviesControllerIntegrationTest {
 				.andExpect(jsonPath("$[?(@.name == 'The Glorious Fool')].actors[*]",
 						Matchers.equalTo(Arrays.asList("Helene Chadwick", "Richard Dix"))));
 	}
+
+	@Test
+    public void testSearchWithEmptyString() throws Exception {
+        mvc.perform(get("/movies/search?q="))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$").isArray());
+    }
 }
